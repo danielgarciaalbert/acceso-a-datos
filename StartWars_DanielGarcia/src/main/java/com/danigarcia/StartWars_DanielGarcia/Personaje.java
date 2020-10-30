577f70448c6e4a596d6ca2906bd3668a786496ae
Daniel García Albert <garciaalbertdaniel@gmail.com>
2020-10-30 20:03:40 +0100

    Updates

--- StartWars_DanielGarcia/src/main/java/com/danigarcia/StartWars_DanielGarcia/Personaje.java
+++ StartWars_DanielGarcia/src/main/java/com/danigarcia/StartWars_DanielGarcia/Personaje.java
@@ -1,30 +1,33 @@
 package com.danigarcia.StartWars_DanielGarcia;
 
 import java.io.Serializable;
-import java.util.List;
 
 class Personaje implements Serializable {
-	String name;
-	String height;
-	String mass;
-	String hair_color;
-	String skin_color;
-	String eye_color;
-	String birth_year;
-	String gender;
-	String homeworld;
-	List<String> films;
-	List<String> species;
-	List<String> vehicles;
-	List<String> starships;
-	String created;
-	String edited;
-	String url;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String name;
+	private String height;
+	private String mass;
+	private String hair_color;
+	private String skin_color;
+	private String eye_color;
+	private String birth_year;
+	private String gender;
+	private String homeworld;
+	private String[] films;
+	private String[] species;
+	private String[] vehicles;
+	private String[] starships;
+	private String created;
+	private String edited;
+	private String url;
 	
 	public Personaje(String name, String height, String mass, String hair_color,
 			String skin_color, String eye_color, String birth_year, 
-			String gender, String homeworld, List<String> films, List<String> species, 
-			List<String> vehicles, List<String> starships, String created, String edited,
+			String gender, String homeworld, String[] films, String[] species, 
+			String[] vehicles, String[] starships, String created, String edited,
 			String url) {
 		this.name = name;
 		this.height = height;
@@ -42,6 +45,190 @@ class Personaje implements Serializable {
 		this.created = created;
 		this.edited = edited;
 		this.url = url;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public String getHeight() {
+		return height;
+	}
+
+
+
+	public void setHeight(String height) {
+		this.height = height;
+	}
+
+
+
+	public String getMass() {
+		return mass;
+	}
+
+
+
+	public void setMass(String mass) {
+		this.mass = mass;
+	}
+
+
+
+	public String getHair_color() {
+		return hair_color;
+	}
+
+
+
+	public void setHair_color(String hair_color) {
+		this.hair_color = hair_color;
+	}
+
+
+
+	public String getSkin_color() {
+		return skin_color;
+	}
+
+
+
+	public void setSkin_color(String skin_color) {
+		this.skin_color = skin_color;
+	}
+
+
+
+	public String getEye_color() {
+		return eye_color;
+	}
+
+
+
+	public void setEye_color(String eye_color) {
+		this.eye_color = eye_color;
+	}
+
+
+
+	public String getBirth_year() {
+		return birth_year;
+	}
+
+
+
+	public void setBirth_year(String birth_year) {
+		this.birth_year = birth_year;
+	}
+
+
+
+	public String getGender() {
+		return gender;
+	}
+
+
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+
+
+	public String getHomeworld() {
+		return homeworld;
+	}
+
+
+
+	public void setHomeworld(String homeworld) {
+		this.homeworld = homeworld;
+	}
+
+
+
+	public String[] getFilms() {
+		return films;
+	}
+
+
+
+	public void setFilms(String[] films) {
+		this.films = films;
+	}
+
+
+
+	public String[] getVehicles() {
+		return vehicles;
+	}
+
+
+
+	public void setVehicles(String[] vehicles) {
+		this.vehicles = vehicles;
+	}
+
+
+
+	public String[] getStarships() {
+		return starships;
+	}
+
+
+
+	public void setStarships(String[] starships) {
+		this.starships = starships;
+	}
+
+
+
+	public String getCreated() {
+		return created;
+	}
+
+
+
+	public void setCreated(String created) {
+		this.created = created;
+	}
+
+
+
+	public String getEdited() {
+		return edited;
+	}
+
+
+
+	public void setEdited(String edited) {
+		this.edited = edited;
+	}
+
+
+
+	public String getUrl() {
+		return url;
+	}
+
+
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+
+
+	public String[] obtenerEspecies() {
+		return species;
 	}
 	
 	@Override
--- StartWars_DanielGarcia/src/main/java/com/danigarcia/StartWars_DanielGarcia/Programa.java
+++ StartWars_DanielGarcia/src/main/java/com/danigarcia/StartWars_DanielGarcia/Programa.java
@@ -8,34 +8,25 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.InvalidClassException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Collectors;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-
 import com.google.gson.Gson;
 
 public class Programa {
@@ -43,10 +34,14 @@ public class Programa {
 		Scanner sc = new Scanner(System.in);
 		String opcion;
 		List<Personaje> personajes = new ArrayList<>();
-		personajes = desSerializarPersonajes("personajes.dat");
+
+		File archivo = new File("personajes.dat");
+		if (archivo.length() > 0 && archivo.exists()) {
+			personajes = deserializarPersonajes("personajes.dat");
+		}
 
 		do {
-			MostrarMenu();
+			mostrarMenu();
 			opcion = sc.nextLine();
 
 			switch (opcion) {
@@ -55,18 +50,11 @@ public class Programa {
 				System.out.print("Introduce el código de una película: ");
 				String codigoPelicula = sc.nextLine();
 				System.out.println();
-				String infoPelicula = leerUrl("https://swapi.dev/api/films/" + codigoPelicula + "/?format=json");
-
-				String infoPeliculaParseada = "";
 				try {
-					Object o = new JSONParser().parse(infoPelicula);
-					JSONObject jo = (JSONObject) o;
-					infoPeliculaParseada = jo.toJSONString();
+					convertir(leerUrl("https://swapi.dev/api/films/" + codigoPelicula + "/?format=json"));
 				} catch (Exception e) {
-					// ERROR
+					System.out.println("La conversión del archivo ha sido cancelada");
 				}
-
-				convertir(infoPeliculaParseada);
 				break;
 			case "2":
 				System.out.println("\nHas elegido la opción 2 (Añadir personaje)...");
@@ -77,21 +65,30 @@ public class Programa {
 				try {
 					p = anyadirPersonaje("https://swapi.dev/api/people/" + codigoPersonaje + "/?format=json");
 
-					if (!personajes.contains(p)) {
-						personajes.add(p);
-						System.out.println("Personaje añadido correctamente.");
+					if (p != null) {
+						if (!personajes.contains(p)) {
+							personajes.add(p);
+							System.out.println("Personaje añadido correctamente.");
+						} else {
+							System.out.println("Este personaje ya ha sido añadido previamente.");
+						}	
 					} else {
-						System.out.println("Este personaje ya ha sido añadido previamente.");
+						System.out.println("No se ha añadido ningún personaje");
 					}
 				} catch (Exception e) {
-					System.out.println("Ese personaje no existe");
+					System.out.println("No se ha añadido ningún personaje");
 				}
 
 				break;
 			case "3":
 				System.out.println("\nHas elegido la opción 3 (Guardar personajes)...");
-				serializarPersonajes(personajes, "personajes.dat");
-				System.out.println("Personajes guardados correctamente");
+				
+				if (personajes.size() > 0) {
+					serializarPersonajes(personajes, "personajes.dat");	
+					System.out.println("Personajes guardados correctamente");
+				} else {
+					System.out.println("No hay ningún personaje añadido");
+				}
 				break;
 			case "4":
 				System.out.println("\nHas elegido la opción 4 (Mostrar especie de personaje)...");
@@ -106,20 +103,18 @@ public class Programa {
 				}
 				break;
 			case "5":
+				System.out.println("\nHas elegido la opción 5 (Mostrar datos de XML)...");
+				System.out.println();
+
+				leerXML("resultado.xml");
 				break;
 			case "6":
 				System.out.println("¡Hasta luego!");
 				break;
-			case "7": // MOSTRAR TEMPORALMENTE LA LISTA DE PERSONAJES
-				for (int i = 0; i < personajes.size(); i++) {
-					System.out.println(personajes.get(i));
-				}
-				break;
 			default:
-				System.out.println("default");
+				System.out.println("Esta opción no es válida.");
 				break;
 			}
-
 			System.out.println();
 		} while (!opcion.equals("6"));
 
@@ -127,9 +122,8 @@ public class Programa {
 	}
 
 	// - MOSTRAR MENU
-	// ----------------------------------------------------------------------------------------------
 
-	private static void MostrarMenu() {
+	private static void mostrarMenu() {
 		System.out.println("***************");
 		System.out.println("** STAR WARS **");
 		System.out.println("***************");
@@ -143,26 +137,7 @@ public class Programa {
 		System.out.print("Elige una opción: ");
 	}
 
-	// - LEER URL
-	// ----------------------------------------------------------------------------------------------
-
-	private static String leerUrl(String web) {
-		try {
-			URL url = new URL(web);
-			URLConnection uc = url.openConnection();
-			uc.setRequestProperty("User-Agent", "PostmanRuntime/7.20.1");
-			uc.connect();
-			String lines = new BufferedReader(new InputStreamReader(uc.getInputStream(), StandardCharsets.UTF_8))
-					.lines().collect(Collectors.joining());
-			return lines;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
 	// - 1. CONVERTIR JSON EN XML
-	// ----------------------------------------------------------------------------------------------
 
 	private static void convertir(String info) {
 		String[] contenido = info.split(",\"");
@@ -206,48 +181,28 @@ public class Programa {
 
 			resultado.close();
 			System.out.println("El archivo se ha generado correctamente.");
-		} catch (IOException errorDeFichero) {
-			System.out.println("Ha habido problemas: " + errorDeFichero.getMessage());
+		} catch (IOException e) {
+			System.out.println("Error al convertir el archivo");
 		}
-
-//		ESTA LÍNEA ES PARA MOSTRAR EN CONSOLA EL RESULTADO
-//		String ultimaEtiqueta = "";
-//		for (int i = 0; i < contenido.length; i++) {
-//			if (etiquetas.contains(contenido[i].split(":")[0])) {
-//				System.out.print("<" + contenido[i].split(":")[0] + "> ");
-//				System.out.print(contenido[i].substring(
-//						contenido[i].split(":")[0].length() + 1));
-//				System.out.println(" </" + contenido[i].split(":")[0] + ">");
-//				
-//				ultimaEtiqueta = contenido[i].split(":")[0];
-//			} else {
-//				
-//				System.out.print("<" + ultimaEtiqueta + "> ");
-//				System.out.print(contenido[i]);
-//				System.out.println(" </" + ultimaEtiqueta + ">");
-//			}
-//		}
 	}
 
 	// - 2. AÑADIR PERSONAJE
-	// ----------------------------------------------------------------------------------------------
 
 	public static Personaje anyadirPersonaje(String url) {
 		try {
 			String json = leerUrl(url);
 
 			Gson gson = new Gson();
-			Personaje p = gson.fromJson(json, Personaje.class);
+			Personaje personaje = gson.fromJson(json, Personaje.class);
 
-			return p;
+			return personaje;
 		} catch (Exception e) {
-			e.printStackTrace();
+			System.out.println("Error al leer información del personaje");
 			return null;
 		}
 	}
 
 	// - 3. SERIALIZAR PERSONAJES
-	// ----------------------------------------------------------------------------------------------
 
 	public static void serializarPersonajes(List<Personaje> personajes, String archivo) {
 		try {
@@ -257,65 +212,114 @@ public class Programa {
 			objectFile.close();
 			archivoSalida.close();
 		} catch (IOException e) {
-			e.printStackTrace();
-			// System.out.println("Error al serializar personajes");
+			System.out.println("Error al serializar personajes");
 		}
 	}
 
 	// - 4. VER ESPECIE DE PERSONAJE
-	// ----------------------------------------------------------------------------------------------
 
 	private static void obtenerEspecies(String codigoPersonaje) {
 		try {
-			List<String> nombreDeEspecies = new ArrayList<>();
 			String json = leerUrl("https://swapi.dev/api/people/" + codigoPersonaje + "/?format=json");
-
 			Gson gson = new Gson();
 			Personaje personaje = gson.fromJson(json, Personaje.class);
 			Especie especie;
 
-			if (!personaje.species.isEmpty()) {
-				for (int i = 0; i < personaje.species.size(); i++) {
-//					System.out.println(personaje.species.get(i) + "?format=json");
-					String newJson = leerUrl(personaje.species.get(i) + "?format=json");
+			if (personaje.obtenerEspecies().length != 0) {
+				for (int i = 0; i < personaje.obtenerEspecies().length; i++) {
+					String newJson = leerUrl(personaje.obtenerEspecies()[i].replace("http", "https") + "?format=json");
 					Gson newGson = new Gson();
 					especie = newGson.fromJson(newJson, Especie.class);
-
+					System.out.println(especie.obtenerNombre());
 				}
 			} else {
 				System.out.println("No hay ninguna especie registrada");
 			}
 
-//			System.out.println("----- PRUEBA -----");
-//			System.out.println(personaje.species);
-//			System.out.println();
-
+		} catch (NullPointerException n) {
+			//Aquí no muestro ningún mensaje porque ya lo muestra la función leerUrl()
 		} catch (Exception e) {
-			e.printStackTrace();
+			System.out.println("Error al obtener las especies del personaje");
+		}
+	}
+
+	// - 5. LEER ARCHIVO XML
+
+	private static void leerXML(String archivo) {
+		try {
+			File archivoEntrada = new File(archivo);
+
+			if (archivoEntrada.length() != 0) {
+				DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+				Document documento = dBuilder.parse(archivoEntrada);
+				documento.getDocumentElement().normalize();
+				NodeList nodos = documento.getElementsByTagName("root");
+
+				for (int i = 0; i < nodos.getLength(); i++) {
+					Node nodo = nodos.item(i);
+
+					if (nodo.getNodeType() == Node.ELEMENT_NODE) {
+						Element elemento = (Element) nodo;
+
+						if (elemento.hasChildNodes()) {
+							NodeList listaDeNodos = nodo.getChildNodes();
+
+							for (int j = 0; j < listaDeNodos.getLength(); j++) {
+								Node otroNodo = listaDeNodos.item(j);
+								if (otroNodo.getNodeName() != "#text")
+									System.out.println(otroNodo.getNodeName() + ": " + otroNodo.getTextContent());
+							}
+						}
+					}
+				}
+			} else {
+				System.out.println("El archivo está vacío o no existe");
+			}
+		} catch (Exception e) {
+			System.out.println("Error al leer el archivo");
 		}
 	}
 
 	// - DESERIALIZAR PERSONAJES
-	// ----------------------------------------------------------------------------------------------
 
-	public static ArrayList<Personaje> desSerializarPersonajes(String archivo) {
-		ArrayList<Personaje> listaPersonajes = new ArrayList<Personaje>();
+	public static ArrayList<Personaje> deserializarPersonajes(String archivo) {
+		ArrayList<Personaje> listaPersonajes = new ArrayList<>();
 		try {
 			FileInputStream archivoEntrada = new FileInputStream(archivo);
 			ObjectInputStream entrada = new ObjectInputStream(archivoEntrada);
-			listaPersonajes = (ArrayList) entrada.readObject();
-			archivoEntrada.close();
+			listaPersonajes = (ArrayList<Personaje>) entrada.readObject();
+			entrada.close();
 			archivoEntrada.close();
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("Archivo no encontrado");
+		} catch (InvalidClassException e) {
+			System.out.println("Clase no válida");
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("Clase no encontrada");
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("Error desconocido");
 		}
 		return listaPersonajes;
+	}
+	
+	// - LEER URL
+
+	private static String leerUrl(String web) {
+		try {
+			URL url = new URL(web);
+			URLConnection uc = url.openConnection();
+			uc.setRequestProperty("User-Agent", "PostmanRuntime/7.20.1");
+			uc.connect();
+			String lines = new BufferedReader(new InputStreamReader(
+					uc.getInputStream(), StandardCharsets.UTF_8))
+					.lines().collect(Collectors.joining());
+			return lines;
+		} catch (FileNotFoundException e) {
+			System.out.println("No se ha encontrado información con ese código.");
+		} catch (Exception e) {
+			System.out.println("Error desconocido al leer el enlace.");
+		}
+		return null;
 	}
 }
